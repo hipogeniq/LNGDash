7e9732df9764680c65475551cb562cd4480a62d1
Catalin Ristache <Catalin_Ristache@waters.com>
2022-12-15 17:56:01 +0200

    remove plotly for now

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,6 @@ import time  # to simulate a real time data, time loop
 
 import numpy as np  # np mean, np random
 import pandas as pd  # read csv, df manipulation
-import plotly.express as px  # interactive charts
 import streamlit as st  # 🎈 data web app development
 from tabulate import tabulate
 from datetime import datetime
